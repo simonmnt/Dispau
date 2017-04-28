c78cd7f8228692be9185acda2b8fab15329594a4
Francois Labastie <flabastie@hotmail.com>
2017-04-28 14:26:57 +0200

    Modifs formulaire

---
+++ groupe2/dispo-mobile/js/form-traitement.js
@@ -0,0 +1,33 @@
+/* form-traitement.js */
+
+$(document).ready(function() {
+
+	$('#form-btn-user-send').click(function(event) {
+	  event.preventDefault();
+	  recupFormData();
+	});
+
+	function recupFormData()
+	{
+		// recup valeur input pseudo_str
+		var pseudo_str = $('#form-pseudo-input').val();
+		console.log(pseudo_str);
+
+		// recup valeur input password_str
+		var password_str = $('#form-password-input').val();
+		console.log(password_str);
+
+		// recup valeur nom_str
+		var nom_str = $('#form-nom-input').val();
+		console.log(nom_str);
+
+		// recup valeur email_str
+		var email_str = $('#form-email-input').val();
+		console.log(email_str);
+
+		// recup valeur file_str
+		var file_str = $('#form-file-input').val();
+		console.log(file_str);
+	}
+
+});
